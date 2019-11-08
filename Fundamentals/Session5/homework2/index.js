882a29bf92d41b4be04d9a9441ccb7c510805efa
Nhu Quynh <you@example.com>
2019-11-08 22:13:59 +0700

    Exercises Function

--- Fundamentals/Session5/homework2/index.js
+++ Fundamentals/Session5/homework2/index.js
@@ -1,15 +1,10 @@
 function calculateSupply(currentAge, maxAge, amountPerDay)
 {
     maxAge = prompt('The Maximum Age:')
-    do{
-        currentAge = prompt('The Current Age:')
-        if(currentAge > maxAge)
-        console.log('Error!')
-        break;
-    }while(parseInt(currentAge > maxAge))
+    currentAge = prompt('The Current Age:')
     amountPerDay = prompt('Amount per day:')
-    let restOfTheLife = ((maxAge - currentAge)*amountPerDay*365)
-    console.log('You will need '+`${restOfTheLife}`+' to last you until the ripe old age of '+`${maxAge}`)
+    let restOfTheLife = ((maxAge - currentAge) * amountPerDay * 365)
+    console.log('You will need ' + `${restOfTheLife}` + ' to last you until the ripe old age of ' + `${maxAge}`)
 }
 calculateSupply()
 calculateSupply()
